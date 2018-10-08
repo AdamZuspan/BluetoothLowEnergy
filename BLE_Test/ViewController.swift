17f8e17f1ce4c84c34ce7238600c76d5ac81d2dc
AdamZuspan <adam.zuspan@mobileappscompany.us>
2018-10-08 11:59:54 -0400

    Initial Commit

---
+++ BLE_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLE_Test
+//
+//  Created by Admin on 10/8/18.
+//  Copyright © 2018 MobileAppsCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
